5bfd9c7259365812de3672a6173c30d1d9ae5277
MOLLY-S1 <sankeym@middleton.school.nz>
2023-04-03 14:28:49 +1200

    Add update

---
+++ 02_Add_V1.py
@@ -0,0 +1,26 @@
+"""Add Version 1
+This Code will store combos in a dictionary"""
+
+import easygui
+
+combos = {"VALUE":
+              {"Beef Burger": 5.69,
+               "Fries": 1.00,
+               "Fizzy Drink": 1.00},
+          "CHEEZY":
+              {"Cheeseburger": 6.69,
+               "Fries": 1.00,
+               "Fizzy Drink":1.00},
+          "SUPER":
+              {"Cheeseburger": 6.69,
+               "Large Fries": 2.00,
+               "Smoothie": 2.00}
+          }
+text = "Enter the listed combo items"
+title = "Combo Enter"
+input_list = ["Combo Name", "Burger", "Side", "Drink"]
+values = []
+enter = easygui.multenterbox(text, title, input_list)
+"Combo name".append(values)
+print(values)
+
