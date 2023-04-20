42e4ccfb5ab10a82a2a014f26509a33b0390d1d8
MOLLY-S1 <sankeym@middleton.school.nz>
2023-04-20 12:44:35 +1200

    Change Update

--- 04_Change_V2.py
+++ 04_Change_V2.py
@@ -10,24 +10,8 @@ def change(confirm_combo):
               f"{ID}\n" f"{combo}", "Combo Check", choices=["Yes", "No"])
 
     if correct == "No":
-        change_value = easygui.buttonbox("What would you like to change?",
-                                         "Change Choice", choices=
-                                         ["Item Name", "Item Price",
-                                          "Combo Name"])
-        if change_value == "Item Name":
-            item = easygui.buttonbox("What item do you want to change:",
-                                     "Item Name", choices=[confirm_combo[combo_ID][key],
-                                                           confirm_combo[combo_ID][key],
-                                                           confirm_combo[combo_ID][key]])
-        if change_value == "Item Price":
-            price = easygui.buttonbox("What price would you like to change: ",
-                                      "Item Price",
-                                      choices=[confirm_combo[combo_ID][key],
-                                               confirm_combo[combo_ID][key],
-                                               confirm_combo[combo_ID][key]])
+        easygui.msgbox("Oh no, please re-enter the combo information")
 
-
-# Main routine
 combos = {"VALUE":
               {"Beef Burger": 5.69,
                "Fries": 1.00,
